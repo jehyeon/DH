f1e9182cb55d162916d2a1a05d196ff98e1045e4
jehyeon <hyeon.rot@gmail.com>
2023-08-18 14:55:23 +0900

    프로젝트 시작

---
+++ Source/DH/DH.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class DH : ModuleRules
+{
+	public DH(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
---
+++ Source/DH/DHGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DHGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADHGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADHGameMode();
+};
+
+
+
